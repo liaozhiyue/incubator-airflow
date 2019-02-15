c9934642974de05c18dc5efbd66a6fb82cc08f40
root <root@bj-hd-airflow-work-01.ali.keep>
2019-02-15 18:50:34 +0800

    优化安装脚本

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@ echo 'uninstall apache-airflow'
 pip uninstall apache-airflow
 echo 'install apache-airflow'
 python setup.py install
-exit 0
+
 current_date=`date "+%Y%m%d%H%m%S"`
-installed_path=/home/airflow/airflow/plugins
-source_path=/home/airflow/airflow-install-code/airflow
-backup_path=/home/airflow/airflow/plugins.bak.${current_date}
+installed_path=${AIRFLOW_HOME}/plugins
+source_path=/disk/m/airflow-code/airflow
+backup_path=${AIRFLOW_HOME}/plugins.bak.${current_date}
 
 echo "backup ${installed_path} to ${backup_path}"
 mv ${installed_path} ${backup_path}
-mkdir /home/airflow/airflow/plugins
+mkdir ${installed_path}
 echo "install plugins: cp to ${source_path}/airflow-dag-creation-manager-plugin/plugins/dcmp to ${installed_path}/"
 cp -r ${source_path}/airflow-dag-creation-manager-plugin/plugins/dcmp ${installed_path}/ 
 echo "install plugins: cp to ${source_path}/airflow-dag-status-manager-plugin/plugins/dag_available_plugin to ${installed_path}/"
